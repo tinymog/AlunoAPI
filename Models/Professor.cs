8afd08741afef86f0efba472bdf5a39a5f5f0a7c
tinymog <hkmplusnow@gmail.com>
2025-08-25 09:02:15 -0300

    passo benço

---
+++ Models/Professor.cs
@@ -0,0 +1,9 @@
+﻿namespace AlunoAPI.Models
+{
+    public class Professor : Base
+    {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? ComponenteCurricular { get; set; }
+    }
+}
